cea3470ae3f1488e205be7ab2c9a72da8bede259
asbelljc <asbelljc@gmail.com>
2021-10-01 01:39:21 -0400

    Added functions to process data from API into a simplified object. Error handling will need to be added and refined but this serves as a solid rough draft.

--- src/weatherDataTools.js
+++ src/weatherDataTools.js
@@ -1,7 +1,7 @@
 import countries from './countries.json';
 import usStates from './us-states.json';
 import usCities from './us-cities.json';
-import getLocalDateAndTime from './timeTools.js';
+import getLocalDateAndTime from './timeTools';
 
 const usCityIds = Array.from(usCities, (city) => city.id);
 const usStateNames = Object.values(usStates);
@@ -17,14 +17,20 @@ async function getBasicDataSource(city, state, country) {
 }
 
 // SOMEWHAT sure that this can be synchronous...
+// Might need additional error handling
 function getState(source) {
-  return usCities.filter((city) => city.id === source.id)[0].state;
+  return source.sys.country === 'US'
+    ? usCities.filter((city) => city.id === source.id)[0].state
+    : null;
 }
 
 async function getComplexDataSource(city, state, country) {
   const basicDataSource = await getBasicDataSource(city, state, country);
   const latitude = basicDataSource.coord.lat;
   const longitude = basicDataSource.coord.lon;
+  const cityFromApi = basicDataSource.name;
+  const stateFromApi = getState(basicDataSource);
+  const countryFromApi = basicDataSource.sys.country;
 
   const response = await fetch(
     `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&exclude=minutely&appid=37ed2f3dbba73d4855aa2f683c7e3232`
@@ -32,12 +38,98 @@ async function getComplexDataSource(city, state, country) {
 
   const complexDataSource = await response.json();
 
+  // This place data fills in gaps in the API response and allows user to
+  // confirm the data is indeed for the city they requested
+  complexDataSource.city = cityFromApi;
+  complexDataSource.state = stateFromApi;
+  complexDataSource.country = countryFromApi;
+
   return complexDataSource;
+}
+
+function getCurrentData(source) {
+  const dateAndTime = getLocalDateAndTime(
+    // might change to use just a property
+    source.current.dt,
+    source.timezone_offset
+  );
+  const iconCode = source.current.weather[0].icon;
+  const temperature = source.current.temp;
+  const feelsLike = source.current.feels_like;
+  const uvIndex = source.current.uvi;
+  const sunrise = getLocalDateAndTime(
+    source.current.sunrise,
+    source.timezone_offset
+  ).fullTime;
+  const sunset = getLocalDateAndTime(
+    source.current.sunset,
+    source.timezone_offset
+  ).fullTime;
+  const windSpeed = source.current.wind_speed;
+  const windDirection = source.current.wind_deg; // don't forget to use DOWN arrow icon
+  const { humidity } = source.current;
+
+  return {
+    dateAndTime,
+    iconCode,
+    temperature,
+    feelsLike,
+    uvIndex,
+    sunrise,
+    sunset,
+    windSpeed,
+    windDirection,
+    humidity,
+  };
+}
+
+function getHourlyData(source) {
+  const next24Hours = source.hourly.slice(0, 24);
+  const hourlyData = next24Hours.map((dataObject) => {
+    const { hour } = getLocalDateAndTime(dataObject.dt, source.timezone_offset);
+    const iconCode = dataObject.weather[0].icon;
+    const temperature = dataObject.temp;
+    const chanceOfPrecip = dataObject.pop;
+
+    return { hour, iconCode, temperature, chanceOfPrecip };
+  });
+
+  return hourlyData;
+}
+
+function getDailyData(source) {
+  const dailyData = source.daily.map((dataObject) => {
+    const { day } = getLocalDateAndTime(dataObject.dt, source.timezone_offset);
+    const iconCode = dataObject.weather[0].icon;
+    const highTemp = dataObject.temp.max;
+    const lowTemp = dataObject.temp.min;
+    const chanceOfPrecip = dataObject.pop;
+
+    return { day, iconCode, highTemp, lowTemp, chanceOfPrecip };
+  });
+
+  return dailyData;
 }
 
 async function getWeatherData(city, state = '', country) {
   const rawData = await getComplexDataSource(city, state, country);
+  const cityName = rawData.city;
+  const stateName = rawData.state;
+  const countryName = rawData.country;
+  const currentData = getCurrentData(rawData);
+  const hourlyData = getHourlyData(rawData);
+  const dailyData = getDailyData(rawData);
+
+  return {
+    city: cityName,
+    state: stateName,
+    country: countryName,
+    current: currentData,
+    hourly: hourlyData,
+    daily: dailyData,
+  };
 }
 
 // NOTES
-// api hourly data starts at most recent hour (eg. if it's now 6:20pm, 6pm)
+// api hourly data starts with most recent hour (eg. if it's now 6:20pm, 6pm)
+// api daily data starts with current day
