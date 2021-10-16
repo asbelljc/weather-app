50b51413266d15674ec8db2873922c36e2d11bac
asbelljc <asbelljc@gmail.com>
2021-10-16 01:17:13 -0400

    Added script to refresh weather data every 10 minutes.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import loadAuxiliary from './auxTile';
 import getLocalDateAndTime from './timeTools';
 
 let currentData; // for caching current locale data
+let refreshInterval; // for auto-updating weather
 
 function clearTiles() {
   const root = document.getElementById('root');
@@ -51,13 +52,16 @@ async function showWeather(city, state, country) {
   loadAuxiliary(currentData);
   addUnitsHandler();
   addLocationHandler();
+
+  clearInterval(refreshInterval);
+  setTimeout(refreshWeather, 0);
 }
 
 function closeModal() {
   const modal = document.querySelector('.modal');
 
   modal.classList.remove('open');
-  setTimeout(() => document.body.removeChild(modal), 0);
+  setTimeout(() => document.body.removeChild(modal), 400);
 }
 
 function showModal() {
@@ -67,6 +71,7 @@ function showModal() {
 
 function addModalHandlers() {
   const modal = document.querySelector('.modal');
+  const modalContent = document.querySelector('.modal-content');
   const searchBtn = document.querySelector('.search-btn');
   const closeBtn = document.querySelector('.close-btn');
   const cityInput = document.querySelector('.city-input');
@@ -80,13 +85,21 @@ function addModalHandlers() {
 
   window.addEventListener('click', (e) => {
     if (e.target === modal && !noWeatherYet) {
-      closeModal();
-    } // close if anywhere outside modal-content is clicked (if weather already shown)
+      closeModal(); // close if anywhere outside modal-content is clicked (unless no weather shown)
+    }
   });
 
   searchBtn.addEventListener('click', () => {
-    showWeather(cityInput.value, stateInput.value, countryInput.value);
-    closeModal();
+    if (!cityInput.value && !countryInput.value) {
+      modalContent.setAttribute('error', 'Please enter a location');
+    } else if (!cityInput.value) {
+      modalContent.setAttribute('error', 'Please enter a city');
+    } else if (!countryInput.value) {
+      modalContent.setAttribute('error', 'Please enter a country');
+    } else {
+      showWeather(cityInput.value.trim(), stateInput.value, countryInput.value);
+      closeModal();
+    }
   });
 }
 
@@ -96,7 +109,16 @@ function openModal() {
   addModalHandlers();
 }
 
+function refreshWeather() {
+  // refresh data every 10 minutes
+  refreshInterval = setInterval(() => {
+    showWeather(currentData.city, currentData.state, currentData.country);
+    console.log('hey');
+  }, 600000);
+}
+
 const refreshClock = setInterval(() => {
+  // refresh clock every 30 seconds
   if (!!currentData) {
     const currentUnixTime = Math.round(Date.now() / 1000); // unix time uses seconds instead of ms
     const dateAndTime = document.getElementsByClassName('date-and-time')[0];
@@ -110,76 +132,9 @@ const refreshClock = setInterval(() => {
 
 openModal();
 
-// getWeatherData('Colorado Springs', 'CO', 'US').then((data) => {
-//   currentData = data; // cache current locale data
-
-//   const background = require(`./Backgrounds/${data.current.iconCode}.jpg`);
-//   document.documentElement.style.backgroundImage = `url(${background})`;
-
-//   loadMain(data);
-//   loadDaily(data);
-//   loadAuxiliary(data);
-//   loadModal();
-//   handleUnits();
-// });
-
-// linear-gradient(to bottom, rgba(255,255,255,1), rgba(255,255,255,0) 50%),
-
-// const usCityIds = Array.from(usCities, (city) => city.id);
-// const usStateNames = Object.values(usStates);
-// const usStateCodes = Object.keys(usStates);
-
-// const body = document.querySelector('body');
-
-// const temperatureBox = document.createElement('div');
-// temperatureBox.className = 'temperature';
-
-// const cityBox = document.createElement('div');
-// cityBox.className = 'city';
-
-// const stateBox = document.createElement('div');
-// stateBox.className = 'state';
-
-// const cityInput = document.createElement('input');
-// cityInput.type = 'text';
-// cityInput.className = 'search-box';
-// cityInput.placeholder = 'Enter your city';
-
-// const stateInput = document.createElement('select');
-// const stateInputDefault = document.createElement('option');
-// stateInputDefault.textContent = 'Select your state';
-// stateInputDefault.value = '';
-// stateInput.appendChild(stateInputDefault);
-// usStateNames.forEach((state) => {
-//   const option = document.createElement('option');
-//   option.textContent = state;
-//   option.value = usStateCodes.filter((code) => usStates[code] === state);
-//   stateInput.appendChild(option);
-// });
-
-// const searchBtn = document.createElement('button');
-// searchBtn.type = 'button';
-// searchBtn.textContent = 'Search';
-// searchBtn.className = 'search-btn';
-// searchBtn.addEventListener('click', () => {
-//   let inputs = cityInput.value.split(',');
-//   inputs = inputs.map((item) => item.trim());
-//   getWeatherData(...inputs);
-// });
-
-// [temperatureBox, cityBox, stateBox, cityInput, stateInput, searchBtn].forEach(
-//   (box) => body.appendChild(box)
-// );
-
-// getWeatherData('boone', 'nc', 'us');
-
 /*
 
 TO DO
-[ ] Make country dropdown box using countries.json
-[ ] Make state dropdown that only activates if USA is selected
-[ ] Make city text input
-[ ] Add error handling for rejected calls and missing data fields
-[ ] Make clock update every minute, weather data every 15 minutes
+[ ] Add error handling for rejected calls
 
 */
