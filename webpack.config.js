f93cb510a9da1616efe9daf18dce2ddd92edbfa2
asbelljc <asbelljc@gmail.com>
2021-10-19 16:54:50 -0400

    Added plugins to minify scripts and css, changed mode to production.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   devtool: 'source-map',
   devServer: {
     static: './dist',
+  },
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new CssMinimizerPlugin({})],
   },
   plugins: [
     new HtmlWebpackPlugin({
